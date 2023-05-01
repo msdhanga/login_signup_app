b00ee34782e2a9313a4292acd9414bfc999019bb
msdhanga <mohammaddhanga2@gmail.com>
2023-05-01 23:25:22 +0530

    second commit

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_app/main.dart';
 
-
 class Main extends StatelessWidget {
 
   @override
@@ -18,116 +17,121 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.lightBlueAccent,
+    return Scaffold(
+        backgroundColor: Colors.black87,
         body: SafeArea(
-          child: Center(
-            child: Form(
-              key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                CircleAvatar(
-                  radius: 55,
-                  backgroundImage: AssetImage('assets/image/cap_ironman.jpg'),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 30 , vertical: 10),
-                  child: TextFormField(
-                   /* validator: (value){
-                      print(value!.length);
-                      if(value!.length != 10){
-                        return 'Name Must be 10 Characters';
-                      }
-                      else{
-                        return 'Required';
-                      }
-                    },*/
-                    decoration: InputDecoration(
-                      fillColor: Colors.black26,
-                      labelText: 'Your Name',
-                      filled: true,
-                    ),
-                    keyboardType: TextInputType. name,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 30 , vertical: 10),
-                  child: TextFormField(
-                    validator: (value){
-                      print(value!.length);
-                      if(value!.length != 10){
-                        return 'Number Must be 10 Characters';
-                      }
-                      else{
-                        return 'Required';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      fillColor: Colors.black26,
-                      labelText: 'Number',
-                      filled: true,
-                    ),
-                    keyboardType: TextInputType.phone,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 30 , vertical: 10),
-                  child: TextFormField(
-                    validator: (value){
-                      print(value!.length);
-                      if(value!.length != 10){
-                        return 'Email Must be @ sign';
-                      }
-                      else{
-                        return 'Required';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      fillColor: Colors.black26,
-                      labelText: 'Email',
-                      filled: true,
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 30 ,vertical: 07),
-                  child: TextFormField(
-                    validator: (value){
-                      print(value!.length);
-                      if(value!.length != 6){
-                        return 'Password Must be 6 Characters';
-                      }
-                      else{
-                        return 'Required';
-                      }
-                    },
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      fillColor: Colors.black38,
-                      labelText: 'Password',
-                      filled: true,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent , fixedSize: Size(150,20)),
-                  onPressed: ()=>{
-                    if(_formKey.currentState!.validate()){
-                      print('Required')
-                    } else {
-                      print('not validate')
-                    }
-                  },
+        child: SingleChildScrollView(
+        child: Center(
+        child: Form(
+        key: _formKey,
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Padding(
+        padding: const EdgeInsets.all(20),
+    child: Text(
+    'Sign Up',
+    style: TextStyle(fontSize: 30,color: Colors.white),
+    ),
+    ),
+    CircleAvatar(
+    radius: 55,
+    backgroundImage: AssetImage('assets/image/cap_ironman.jpg'),
+    ),
+    Container(
+    margin: EdgeInsets.symmetric(horizontal: 30 , vertical: 10),
+    child: TextFormField(
+    decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    fillColor: Colors.white,
+    labelText: 'Your Name',
+    filled: true,
+    ),
+    keyboardType: TextInputType. name,
+    ),
+    ),
+    Container(
+    margin: EdgeInsets.symmetric(horizontal: 30 , vertical: 10),
+    child: TextFormField(
+    validator: (value){
+    print(value!.length);
+    if(value!.length != 10){
+    return 'Number Must be 10 Characters';
+    }
+    else{
+    return null;
+    }
+    },
+    decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    prefixIcon: Icon(Icons.phone),
+    prefixIconColor: Colors.green,
+    fillColor: Colors.white,
+    labelText: 'Number',
+    filled: true,
+    ),
+    keyboardType: TextInputType.phone,
+    ),
+    ),
+    Container(
+    margin: EdgeInsets.symmetric(horizontal: 30 , vertical: 10),
+    child: TextFormField(
+    validator: (value) {
+    if (value!.isEmpty) {
+    return 'Please enter your email';
+    }
+    bool isValid = RegExp(
+    r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+        .hasMatch(value);
+    if (!isValid) {
+    return 'Please enter a valid email';
+    }
+    return null;
+    },
+    decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    prefixIcon: Icon(Icons.email),
+    prefixIconColor: Colors.red,
+    fillColor: Colors.white,
+    labelText: 'Email',
+    filled: true,
+    ),
+    keyboardType: TextInputType.emailAddress,
+    ),
+    ),
+    Container(
+    margin: EdgeInsets.symmetric(horizontal: 30 ,vertical: 07),
+    child: TextFormField(
+    validator: (value){
+    print(value!.length);
+    if(value!.length != 6){
+    return 'Password Must be 6 Characters';
+    }
+    else{
+    return null;
+    }
+    },
+    keyboardType: TextInputType.text,
+    decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    prefixIcon: Icon(Icons.password),
+    prefixIconColor: Colors.blue,
+    fillColor: Colors.white,
+    labelText: 'Password',
+    filled: true,
+    ),
+    ),
+    ),
+    ElevatedButton(
+    style: ElevatedButton.styleFrom(
+    backgroundColor: Colors.redAccent , fixedSize: Size(150,20)),
+    onPressed: (){
+    if(_formKey.currentState!.validate()){
+    print('Required');
+    } else {
+    print('not validate');
+    }
+    },
                   child: Text(
                     'Submit',
                     style: TextStyle(fontSize: 22),
@@ -137,18 +141,22 @@ class SignUp extends StatelessWidget {
                   onTap: (){
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => Login_app()),
+                      MaterialPageRoute(builder: (context) => LoginApp()),
                     );
                   },
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(fontSize: 25),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(fontSize: 15,color: Colors.blue),
+                    ),
                   ),
                 )
               ],
             ),
            )
           ),
+        ),
         ),
       );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,118 +2,130 @@ import 'package:flutter/material.dart';
 
 import 'SignUp.dart';
 
-void main(){
+void main() {
   runApp(Main());
 }
 
 class Main extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login_app(),
+      home: LoginApp(),
     );
   }
 }
 
-class Login_app extends StatelessWidget {
-  Login_app({Key? key}) : super(key: key);
+class LoginApp extends StatelessWidget {
+  LoginApp({Key? key}) : super(key: key);
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-     return  Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: SafeArea(
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: SingleChildScrollView(
           child: Center(
             child: Form(
-             key: _formKey,
+              key: _formKey,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                 Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                CircleAvatar(
-                  radius: 75,
-                  backgroundImage: AssetImage('assets/image/cap.jpg'),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 30 , vertical: 10),
-                  child: TextFormField(
-                    validator: (value){
-                      print(value!.length);
-                      if(value!.length != 10){
-                        return 'Number Must be 10 Characters';
-                    }
-                      else{
-                        return 'Required';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      fillColor: Colors.black26,
-                      labelText: 'Enter Number',
-                      filled: true,
-                    ),
-                    keyboardType: TextInputType.phone,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 30 ,vertical: 07),
-                  child: TextFormField(
-                    validator: (value){
-                      print(value!.length);
-                      if(value!.length != 6){
-                        return 'Password Must be 6 Characters';
-                      }
-                      else{
-                        return 'Required';
-                      }
-                    },
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: InputDecoration(
-                      fillColor: Colors.black38,
-                      labelText: 'Password',
-                      filled: true,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent , fixedSize: Size(150,20)),
-                  onPressed: () => {
-                    if(_formKey.currentState!.validate()){
-                      print('Required')
-                    } else {
-                      print('not validate')
-                    }
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 22),
-                  ),
-                ),
-                InkWell(
-                  onTap: (){
-                    Navigator.push(
-                        context, 
-                        MaterialPageRoute(builder: (context) => SignUp()),
-                    );
-                  },
-                  child: const Text(
-                    'Sign Up',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                )
-              ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+              Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                'Login',
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
             ),
-           ),
+            CircleAvatar(
+              radius: 55,
+              backgroundImage: AssetImage('assets/image/cap.jpg'),
+            ),
+            Container(
+              margin:
+              EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: TextFormField(
+                validator: (value) {
+                  print(value!.length);
+                  if (value!.length != 10) {
+                    return 'Number Must be 10 Characters';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.phone),
+                  prefixIconColor: Colors.green,
+                  fillColor: Colors.white,
+                  labelText: 'Enter Number',
+                  filled: true,
+                ),
+                keyboardType: TextInputType.phone,
+              ),
+            ),
+            Container(
+              margin:
+              EdgeInsets.symmetric(horizontal: 30, vertical: 07),
+              child: TextFormField(
+                obscureText: true,
+                validator: (value) {
+                  print(value!.length);
+                  if (value!.length != 6) {
+                    return 'Password Must be 6 Characters';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.password),
+                  prefixIconColor: Colors.blue,
+                  fillColor: Colors.white,
+                  labelText: 'Password',
+                  filled: true,
+                ),
+              ),
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
+                    fixedSize: Size(150, 20)),
+                onPressed: () => {
+                if (_formKey.currentState!.validate()) {
+                print('Required')
+                }
+                else {
+                print('not validate')
+                }
+          },
+            child: Text(
+              'Login',
+              style: TextStyle(fontSize: 22),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SignUp()),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 15, color: Colors.blue),
+              ),
+            ),
           )
+          ],
         ),
-      );
+      ),
+    ),
+    ),
+    ),
+    );
   }
 }
-
